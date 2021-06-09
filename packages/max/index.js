2afd30337c6f66e596519c0ec112b17ad4af08d7
John Bampton <jbampton@gmail.com>
2021-06-10 08:32:03 +1000

    Add Workers

---
+++ packages/max/index.js
@@ -0,0 +1,16 @@
+const html = `<!doctype html>
+<body>
+  <h1>Hello Max The Super One!!!</h1>
+</body>`
+
+async function handleRequest(request) {
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  })
+}
+
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest(event.request))
+})
