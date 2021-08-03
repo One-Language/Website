bd9b0053582928fab63c4de6f804d0296c2f4525
muuhab <mohab.barca@gmail.com>
2021-08-03 19:13:01 +0200

    First Section created

--- next.config.js
+++ next.config.js
@@ -1,14 +1,36 @@
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
+      ExtractTextPlugin = require("extract-text-webpack-plugin"),
+      path = require("path");
+
 module.exports = {
-  reactStrictMode: true,
-  webpack5: false,
-loaders: [{
-      test: /.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.css$/,
-      loader: "style-loader!css-loader"
-    }, {
-      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-      loader: 'url-loader?limit=100000' }]
+  "entry": ["./app/index.js"],
+  "output": {
+    "path": "output",
+    "filename": "[name]-[chunkhash].js",
+    "libraryTarget": "umd"
+  },
+  "module": {
+    rules: [
+      {
+        "exclude": "/node_modules/",
+        "include": "./app/",
+        "loader": "babel-loader",
+        "options": {
+          "presets": ["es2015", "react"]
+        },
+        "test": /\.jsx?$/
+      },
+      {
+        "exclude": "/node_modules/",
+        "use": ExtractTextPlugin.extract({
+          "fallbackLoader": "style-loader",
+          "loader": ["css-loader", "sass-loader"]
+        }),
+        "test": /\.scss$/
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("[name]-[contenthash].css")
+  ]
 }
--- pages/componenets/FirstSection/FirstSection.js
+++ pages/componenets/FirstSection/FirstSection.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from "react";
 import styles from "./FirstSection.module.css";
 import Image from "next/image";
-
+import { Col,Row,Container } from "react-bootstrap";
 import hljs from "highlight.js";
 
-import Zero from "../../../public/images/0.png";
-// import one from "../../../public/images/1.png";
-// import two from "../../../public/images/2.png";
-// import three from "../../../public/images/3.png";
-// import four from "../../../public/images/4.png";
-// import five from "../../../public/images/5.png";
-// import Group from "../../../public/images/Group.png";
-// import Girl from "../../../public/images/girl.png";
+import zero from "../../../public/images/0.png";
+import one from "../../../public/images/1.png";
+import two from "../../../public/images/2.png";
+import three from "../../../public/images/3.png";
+import four from "../../../public/images/4.png";
+import five from "../../../public/images/5.png";
+import Group from "../../../public/images/Group.png";
+import Girl from "../../../public/images/girl.png";
 
 const FirstSection = () => {
   useEffect(() => {
@@ -26,8 +26,8 @@ const FirstSection = () => {
           it easy to build reliable, efficient and performant software.
         </h4>
       </header>
-        <pre className={styles.test}>
-          <code className={`${styles.test3} js`}>
+        <pre>
+          <code className={`${styles.code} js`}>
           {`fetch('{url}')
     .then(response => console.log(response));
 `}
@@ -40,20 +40,33 @@ const FirstSection = () => {
       <small className={styles.small}>
         Free 30-days trial, no credit card required
       </small>
-      <div className={styles.images}>
-        <ul>
-          <li>{/* <Image src={zero} /> */}</li>
-          <li>{/* <Image src={one} /> */}</li>
-          <li>{/* <Image src={two} /> */}</li>
-          <li>{/* <Image src={three} /> */}</li>
-          <li>{/* <Image src={four} /> */}</li>
-          <li>{/* <Image src={five} /> */}</li>
-        </ul>
-      </div>
+        <Container className={styles.images}>
+          <Row>
+            <Col xs={4} md={2}>
+            <Image src={zero} />
+            </Col>
+            <Col xs={4} md={2}>
+            <Image src={one} />
+            </Col>
+            <Col xs={4} md={2}>
+            <Image src={two} />
+            </Col>
+            <Col xs={4} md={2}>
+            <Image src={three} />
+            </Col>
+            <Col xs={4} md={2}>
+            <Image src={four} />
+            </Col>
+            <Col xs={4} md={2}>
+            <Image src={five} />
+            </Col>
+          </Row>
+        </Container>
+   
       <p className={styles.text2}>
         Their teams and hundreds more are using Popwork - in person or remote.
       </p>
-      <div className={styles.vector}>{/* <Image  src={Group}/> */}</div>
+      <div className={styles.vector}><Image  src={Group}/></div>
       <div className={styles.box2}>
         <p>
           Every week, Popwork asks a few simple questions to your team about
@@ -64,7 +77,7 @@ const FirstSection = () => {
           share feedback and follow up on topics week after week.
         </p>
       </div>
-      <div className={styles.girl}>{/* <Image src={Girl} /> */}</div>
+      <div className={styles.girl}><Image src={Girl} /></div>
     </div>
   );
 };
